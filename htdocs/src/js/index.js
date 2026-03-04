27c6894de73a7d139d11333b21757bf4cd8d961b
ent-tech-julaya <julaya@entacl.com>
2026-03-04 18:18:10 +0900

    add: エラー画面の実装

--- htdocs/src/js/index.js
+++ htdocs/src/js/index.js
@@ -1,4 +1,6 @@
 import './device.js';
 import $ from 'jquery';
 
-const loading = require('./common/loading.js');
+if (document.body.dataset.page !== 'error') {
+  const loading = require('./common/loading.js');
+}
