f8331bf7a0331b01720b93ec3a51ab21301b82ce
ent-tech-julaya <julaya@entacl.com>
2026-03-05 15:51:31 +0900

    add: json読み込みの実装

--- htdocs/src/js/index.js
+++ htdocs/src/js/index.js
@@ -1,6 +1,7 @@
 import './device.js';
 import $ from 'jquery';
 
-if (document.body.dataset.page !== 'error') {
+if (document.body.dataset.page == 'top') {
   const loading = require('./common/loading.js');
+  const users = require('./users.js');
 }
